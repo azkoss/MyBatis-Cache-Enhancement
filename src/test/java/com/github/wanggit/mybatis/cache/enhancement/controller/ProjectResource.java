49ac304e53ab6c86e72003ab13f4593aaa87a29c
wanggang <wanggang@wangg_mail@163.com>
2017-09-20 18:46:48 +0800

    自动脚本生成的文件

---
+++ src/test/java/com/github/wanggit/mybatis/cache/enhancement/controller/ProjectResource.java
@@ -0,0 +1,44 @@
+package com.github.wanggit.mybatis.cache.enhancement.controller;
+
+import java.util.*;
+import javax.servlet.http.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import com.github.wanggit.mybatis.cache.enhancement.service.ProjectService;
+
+
+@Controller
+@RequestMapping("/project")
+public class ProjectResource {
+
+	@Autowired
+	private ProjectService rojectService;
+
+	@PostMapping("/save")
+	@ResponseBody
+	public String save(){
+		 return null;
+	}
+
+	@PostMapping("/update")
+	@ResponseBody
+	public String update(){
+		return null;
+	}
+
+	@GetMapping("/list")
+	@ResponseBody
+	public String list(){
+		return null;
+	}
+
+	@PostMapping("/delete")
+	@ResponseBody
+	public String delete(){
+		return null;
+	}
+
+}
