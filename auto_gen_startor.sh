595119f72b619193edc64328257f8ce91af04285
wanggang <wanggang@wangg_mail@163.com>
2017-09-20 18:47:08 +0800

    自动脚本

---
+++ auto_gen_startor.sh
@@ -0,0 +1,2 @@
+!#/bin/bash
+./autogen.sh src/test/java/com/github/wanggit/mybatis/cache/enhancement/dao/mapper/ src/test/java/com/github/wanggit/mybatis/cache/enhancement/service src/test/java/ com.github.wanggit.mybatis.cache.enhancement.controller
---
+++ mapper2service.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ "$1" = 'help' ]
+then
+	echo "Example:"
+	echo './mapper2service.sh src/main/java/com/fantong/chifaner/system/mapper/PmsTransferAccountMapper.java src/main/java/com/fantong/chifaner/system/service/PmsTransferAccountService.java'
+	exit 0
+fi
+file=$1
+tofile=$2
+service_dir=$3
+service_dir=${service_dir//\//.}
+cp -v $file $tofile
+sed -i "s/^package .*;$/package $service_dir;/" $tofile
+sed -i 's/mapper/service/' $tofile
+sed -i 's/Mapper/Service/' $tofile
+sed -i 's/import org.apache.ibatis.annotations.Param;//' $tofile
+sed -i 's/@Param("record") //g' $tofile 
+sed -i 's/@Param("example")//g' $tofile
