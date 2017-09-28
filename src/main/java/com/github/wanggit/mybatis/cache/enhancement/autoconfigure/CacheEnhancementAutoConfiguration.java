965f022b996f0ce5a4a86fc7bfe5aa0137c0a245
wanggang <wanggang@wangg_mail@163.com>
2017-09-28 14:25:09 +0800

    添加自动配置

--- src/main/java/com/github/wanggit/mybatis/cache/enhancement/autoconfigure/CacheEnhancementAutoConfiguration.java
+++ src/main/java/com/github/wanggit/mybatis/cache/enhancement/autoconfigure/CacheEnhancementAutoConfiguration.java
@@ -1,10 +1,21 @@
 package com.github.wanggit.mybatis.cache.enhancement.autoconfigure;
 
 import com.github.wanggit.mybatis.cache.enhancement.config.CacheEnhancementBeanPostProcessor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.SerializationException;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.*;
 
 @Configuration
 @EnableConfigurationProperties(CacheEnhancementProperties.class)
@@ -14,6 +25,91 @@ public class CacheEnhancementAutoConfiguration {
     @Bean
     public CacheEnhancementBeanPostProcessor cacheEnhancementBeanPostProcessor(){
         return new CacheEnhancementBeanPostProcessor();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "cache.enhancement", value = "redis.enable", havingValue = "true")
+    public RedisTemplate<String, Object> cacheEnhancementRedisTemplate(RedisConnectionFactory redisConnectionFactory
+            , RedisSerializer redisSerializer){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(redisSerializer);
+        redisTemplate.setHashKeySerializer(redisSerializer);
+        redisTemplate.setHashValueSerializer(redisSerializer);
+        return redisTemplate;
+    }
+
+    @Bean
+    @ConditionalOnBean(name = "cacheEnhancementRedisTemplate", value = RedisTemplate.class)
+    @ConditionalOnProperty(prefix = "cache.enhancement", value = "redis.enable", havingValue = "true")
+    @ConditionalOnExpression("'${cache.enhancement.redis.serializer}'.equals('org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer')")
+    public Jackson2JsonRedisSerializer<Object> cacheEnhancementJackson2JsonRedisSerializer(){
+        return new Jackson2JsonRedisSerializer(Object.class);
+    }
+
+    @Deprecated
+    public static class XHashValueSerializer implements RedisSerializer<Object>{
+
+        @Override
+        public byte[] serialize(Object o) throws SerializationException {
+            ByteArrayOutputStream out = null;
+            ObjectOutputStream output = null;
+            try {
+                out = new ByteArrayOutputStream();
+                output = new ObjectOutputStream(out);
+                output.writeObject(o);
+                return out.toByteArray();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if (null != out){
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (null != output){
+                    try {
+                        output.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return new byte[0];
+        }
+
+        @Override
+        public Object deserialize(byte[] bytes) throws SerializationException {
+            ByteArrayInputStream in = null;
+            ObjectInputStream input = null;
+            try {
+                in = new ByteArrayInputStream(bytes);
+                input = new ObjectInputStream(in);
+                return input.readObject();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }finally {
+                if (null != in){
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (null != input){
+                    try {
+                        input.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
     }
 
 }
--- src/main/java/com/github/wanggit/mybatis/cache/enhancement/autoconfigure/CacheEnhancementProperties.java
+++ src/main/java/com/github/wanggit/mybatis/cache/enhancement/autoconfigure/CacheEnhancementProperties.java
@@ -2,12 +2,14 @@ package com.github.wanggit.mybatis.cache.enhancement.autoconfigure;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = CacheEnhancementProperties.MYBATIS_PREFIX)
+@ConfigurationProperties(prefix = CacheEnhancementProperties.CACHE_ENHANCEMENT)
 public class CacheEnhancementProperties {
 
-    public static final String MYBATIS_PREFIX = "cache.enhancement";
+    public static final String CACHE_ENHANCEMENT = "cache.enhancement";
 
     private boolean enable;
+
+    private Redis redis;
 
     public boolean isEnable() {
         return enable;
@@ -15,5 +17,34 @@ public class CacheEnhancementProperties {
 
     public void setEnable(boolean enable) {
         this.enable = enable;
+    }
+
+    public Redis getRedis() {
+        return redis;
+    }
+
+    public void setRedis(Redis redis) {
+        this.redis = redis;
+    }
+
+    public static class Redis{
+        private boolean enable;
+        private String serializer;
+
+        public boolean isEnable() {
+            return enable;
+        }
+
+        public void setEnable(boolean enable) {
+            this.enable = enable;
+        }
+
+        public String getSerializer() {
+            return serializer;
+        }
+
+        public void setSerializer(String serializer) {
+            this.serializer = serializer;
+        }
     }
 }
