6267c3b1332abd18932e761c6a27812083798638
wanggang <wanggang@wangg_mail@163.com>
2017-09-29 16:02:54 +0800

    设置使用App启动测试时的ActiveProfiles

--- src/test/java/com/github/wanggit/mybatis/cache/enhancement/App.java
+++ src/test/java/com/github/wanggit/mybatis/cache/enhancement/App.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootApplication
 @ServletComponentScan
 @MapperScan("com.github.wanggit.mybatis.cache.enhancement.dao.mapper")
 @ComponentScan
+@WebAppConfiguration
 public class App {
 
     public static void main(String[] args) {
